c81cb3ab54e4d10dd776f3d8307c9d75654844e9
Lakmali-Ariyadasa <madhushika.xplosa@gmail.com>
2024-03-20 14:31:45 +0530

    server

--- main_node_project/index.js
+++ main_node_project/index.js
@@ -1,50 +1,7 @@
 const { exec } = require('child_process');
-var yourscript = exec('D:/Lakmali/DC_Project/system_node_project/hi.sh',
-        (error, stdout, stderr) => {
-            console.log(stdout,"stdout1");
-            console.log(stderr,"stderr1");
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-    });
 
-const http = require('http');
 const portfinder = require('portfinder');
 
-// Function to start a server on a specified port
-function startServer(port) {
-    const server = http.createServer((req, res) => {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(`Hello, world! Server running on port ${port}\n`);
-    });
-
-    server.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
-}
-
-// Function to find and start the next five available ports
-function findNextFivePorts(startingPort) {
-    const numberOfPorts = 5;
-    let currentPort = startingPort;
-
-    for (let i = 0; i < numberOfPorts; i++) {
-        portfinder.getPort({
-            port: currentPort
-        }, (err, port) => {
-            if (err) {
-                console.error('Error finding port:', err);
-                return;
-            }
-            startServer(port);
-        });
-
-        // Increment currentPort for the next iteration
-        currentPort++;
-    }
-}
-
-// Call findNextFivePorts function with a starting port
 
 portfinder.getPort((err, port) => {
     if (err) {
@@ -53,5 +10,14 @@ portfinder.getPort((err, port) => {
     }
 
     const startingPort = port; 
-    findNextFivePorts(startingPort);
+
+var scriptSystem = exec(`node D:/Lakmali/DC_Project/system_node_project/index.js ${startingPort}`, 
+        (error, stdout, stderr) => {
+            console.log(stdout,"stdout1");
+            console.log(stderr,"stderr1");
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+    });
+
 })
--- system_node_project/index.js
+++ system_node_project/index.js
@@ -1,2 +1,9 @@
-var shell = require('shelljs');
-shell.echo('hello world');
+const args = process.argv;
+
+// Skipping the first two default arguments (node and the script filename)
+const passedArgs = args.slice(2);
+
+// Assuming that you're passing a single variable value
+const variableValue = passedArgs[0];
+
+console.log("Passed variable value:", variableValue);
