c02b71669692258377380b408dd10e3b4e9ba117
Lakmali-Ariyadasa <madhushika.xplosa@gmail.com>
2024-03-27 13:28:24 +0530

    node table

--- system_node_project/index.js
+++ system_node_project/index.js
@@ -4,6 +4,28 @@ const args = process.argv;
 const passedArgs = args.slice(2);
 
 // Assuming that you're passing a single variable value
-const variableValue = passedArgs[0];
+const nodeValue = passedArgs[0];
 
-console.log("Passed variable value:", variableValue);
+console.log("Passed variable value:", nodeValue);
+
+let nodeTable = [];
+let valueTable = {};
+
+  
+const  nodeName  = nodeValue;
+
+const newNode = { nodeName };
+  
+  nodeTable.push(newNode);
+
+// Determine role based on the number of nodes
+  
+  if (nodeTable.length % 2 === 0) {
+      newNode.role = 'hasher';
+       
+  } else {
+      newNode.role = 'receiver';
+    
+  }
+console.log("nodeTable", nodeTable);
+//   console.log(`Node ${nodeName} with ID ${nodeId} joined as ${newNode.role}`);
